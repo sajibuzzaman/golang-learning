badac932587b23d5f92b92f167d8250202190a49
sajibuzzaman-devsnest <sajib@devsnest.net>
2025-01-28 18:32:58 +0600

    update

---
+++ GoRoutines/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	// "github.com/sajibuzzaman/goroutines/basicGoRoutines"
+		// "github.com/sajibuzzaman/goroutines/mutexAndWaitGroup"
+	"github.com/sajibuzzaman/goroutines/channels"
+
+)
+
+func main() {
+	// basicGoRoutines.BasicGoRoutines()
+	// mutexAndWaitGroup.MutexAndWaitGroup()
+	channels.Channels()
+
+}
